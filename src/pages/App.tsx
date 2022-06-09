ee09487248c43ffca0749e6065c66d8894091de6
CarlosFelixxs <carlos.fsilva@bandtec.com.br>
2022-06-09 14:04:09 -0300

    Botões

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -9,14 +9,19 @@ function App() {
   return (
     <div className="App">
       <div className="container">
-        <Logo linkImagem={imagemLogo} ></Logo>
+        <Logo linkImagem={imagemLogo}></Logo>
         <div className="container2">
           <div className='divTitle'>
             <img src={icon} alt="software team" height={"50px"} width={"50px"} />
-            <b className='title'>NEW GEN PHOTOBOOTH </b><br /><b className='subTitle'>A Liquid Studios project</b>
+            <b className='title'>NEW GEN PHOTOBOOTH </b><br /><b className='title'>A Liquid Studios project</b>
             <List />
           </div>
           <img src={img} alt="software team" height={"250px"} width={"400px"} />
+        </div>
+        <div className='containerButton'>
+          <button>oi</button>
+          <button>oi2</button>
+          <button>oi3</button>
         </div>
       </div>
     </div>
