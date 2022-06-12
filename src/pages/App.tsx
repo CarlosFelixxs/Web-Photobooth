34642d92212b5d8059ca0d0f86427db708d4490c
CarlosFelixxs <carlos.fsilva@bandtec.com.br>
2022-06-11 21:53:56 -0300

    Responsividade

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -5,6 +5,7 @@ import imagemLogo from "../img/logo-AI.png";
 import img from "../img/work.png";
 import icon from "../img/icon1.png";
 import { link } from 'fs';
+import { findAllByDisplayValue, getByDisplayValue } from '@testing-library/react';
 
 function App() {
   return (
@@ -26,7 +27,14 @@ function App() {
           <a href="https://newsroom.accenture.com/news/accenture-cloud-first-launches-with-3-billion-investment-to-accelerate-clients-move-to-cloud-and-digital-transformation.htm"><span id='textButton'>ACCENTURE'S<br />CLOUD FIRST</span></a>
           <a href="https://www.accenture.com/br-pt/insights/technology/technology-trends-2022?c=acn_glb_technologyvisiogoogle_12886377&n=psgs_0322&gclid=Cj0KCQjw-daUBhCIARIsALbkjSbYfZwI_fAVY3l76llH7w8DmDFK"><span id='textButton'>TECH VISION<br /> 2022</span></a>
         </div>
+        <div id='findUs'>
+          <a id='fCont' href="#continuacao"><span>FIND US</span></a>
+      <section className='continuacao'>
+
+      </section>
+        </div>
       </div>
+    
     </div>
   );
 }
