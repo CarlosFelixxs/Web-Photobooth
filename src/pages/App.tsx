045b093ca56af4b23232a161c46d2350cd59582e
gitsena <andre.sena@bandtec.com.br>
2022-06-13 11:25:05 -0300

    adicionando Botão down

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,13 +1,6 @@
 import React, { Component } from 'react';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import List from '../components/List';
-import Logo from '../components/Logo/itemLogo';
-import imagemLogo from "../img/logo-AI.png";
-import img from "../img/work.png";
-import icon from "../img/icon1.png";
-import { link } from 'fs';
 import LinkedinScreen from "../components/LinkedinScreen";
-import { findAllByDisplayValue, getByDisplayValue } from '@testing-library/react';
 import Home from "../components/Home";
 
 function App() {
